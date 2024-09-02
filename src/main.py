f5425f6e7242bb7466e78ce0bd2744e54fef9a0f
manquobi <manquobi@gmail.com>
2024-09-02 19:24:51 +0000

    creating decision tree

--- src/main.py
+++ src/main.py
@@ -1,5 +1,41 @@
-import pandas
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-df = pandas.read_csv("data.csv")
+import sys
+import matplotlib
+#matplotlib.use('Agg')
+
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+df = pandas.read_csv("data/dt.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
 
 print(df) 
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+tree.plot_tree(dtree, feature_names=features) 
+
+#plt.savefig('decision_tree.png')
+#sys.stdout.flush()
+plt.show()  # This will display the plot in VS Code
+
+ 
+
+
+
+
+
