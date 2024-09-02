6bef4a294e24fc7894f69a8fc5c7ed394c1705ac
manquobi <manquobi@gmail.com>
2024-09-02 22:01:05 +0000

    confusion matrics

--- src/main.py
+++ src/main.py
@@ -1,41 +1,19 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-import sys
-import matplotlib
-#matplotlib.use('Agg')
-
-import pandas
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
+import numpy
+from sklearn import metrics
 import matplotlib.pyplot as plt
 
-df = pandas.read_csv("data/dt.csv")
 
-d = {'UK': 0, 'USA': 1, 'N': 2}
-df['Nationality'] = df['Nationality'].map(d)
-d = {'YES': 1, 'NO': 0}
-df['Go'] = df['Go'].map(d)
+actual = numpy.random.binomial(1, 0.9, size = 1000)
+predicted = numpy.random.binomial(1, 0.9, size = 1000) 
+confusion_matrix = metrics.confusion_matrix(actual, predicted) 
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
 
-print(df) 
-
-features = ['Age', 'Experience', 'Rank', 'Nationality']
-
-X = df[features]
-y = df['Go']
-
-dtree = DecisionTreeClassifier()
-dtree = dtree.fit(X, y)
-
-tree.plot_tree(dtree, feature_names=features) 
-
-#plt.savefig('decision_tree.png')
-#sys.stdout.flush()
-plt.show()  # This will display the plot in VS Code
-
-print(dtree.predict([[40, 10, 7, 1]])) 
-
- 
+cm_display.plot()
+plt.show() 
+Accuracy = metrics.accuracy_score(actual, predicted) 
 
 
 
