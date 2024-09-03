845178a8ef84f8ec3592a929e3c23241e4ca7ab2
manquobi <manquobi@gmail.com>
2024-09-03 14:37:16 +0000

    python lists

--- src/main.py
+++ src/main.py
@@ -1,23 +1,101 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
-import numpy
-from sklearn import metrics
-import matplotlib.pyplot as plt
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
 
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
 
-actual = numpy.random.binomial(1, 0.9, size = 1000)
-predicted = numpy.random.binomial(1, 0.9, size = 1000) 
-#confusion_matrix = metrics.confusion_matrix(actual, predicted) 
-#cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
 
-#cm_display.plot()
-#plt.show() 
-Accuracy = metrics.accuracy_score(actual, predicted)    # True Positive + True Negative) / Total Predictions
-Precision = metrics.precision_score(actual, predicted)  # True Positive / (True Positive + False Positive) 
-Sensitivity_recall = metrics.recall_score(actual, predicted) # True Positive / (True Positive + False Negative)
-Specificity = metrics.recall_score(actual, predicted, pos_label=0)  #True Negative / (True Negative + False Positive)
-F1_score = metrics.f1_score(actual, predicted) #2 * ((Precision * Sensitivity) / (Precision + Sensitivity))
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
 
-#print({"Accuracy: " Accuracy,"Precision: " Precision,"Sensitivity_recall: " Sensitivity_recall,"Specificity: " Specificity,"F1_score: " F1_score})
-print({"Accuracy":Accuracy,"Precision":Precision,"Sensitivity_recall":Sensitivity_recall,"Specificity":Specificity,"F1_score":F1_score}) #look into this
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+#print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+#thislist = ["apple", "banana", "cherry"]
+#for x in thislist:
+  #print(x) 
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i]) 
+
+  thislist = ["mango", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  thislist = ["kiwi", "banana", "cherry"]
+[print(x) for x in thislist] 
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist) 
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits]
+
+print(newlist)
+
+print([x for x in range(10)])
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x.upper() for x in fruits] 
+print(newlist)
+
+newlist = ['hello' for x in fruits] 
+print(newlist)
+
+newlist = [x if x != "banana" else "orange" for x in fruits] 
+print(newlist)
+
