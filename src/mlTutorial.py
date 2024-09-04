fe9ccb48463135da7654503e7141e2d4f9b598cb
manquobi <manquobi@gmail.com>
2024-09-04 21:40:29 +0000

    heirarchical clustering- end

--- src/mlTutorial.py
+++ src/mlTutorial.py
@@ -4,13 +4,17 @@ os.system('cls' if os.name == 'nt' else 'clear')
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
 
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 
 data = list(zip(x, y))
+print(data)
 
-linkage_data = linkage(data, method='ward', metric='euclidean')
-dendrogram(linkage_data)
+hierarchical_cluster = AgglomerativeClustering(n_clusters=2, metric='euclidean', linkage='ward')
+labels = hierarchical_cluster.fit_predict(data) 
+print(labels)
 
+plt.scatter(x, y, c=labels)
 plt.show() 
