cbec615289d079ee1ee0f9b272ef410354572b19
manquobi <manquobi@gmail.com>
2024-09-07 11:36:55 +0000

    LOOPS

--- src/main.py
+++ src/main.py
@@ -1,60 +1,85 @@
-
-
-
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
 
+os.system("cls" if os.name == "nt" else "clear")
 
-a = 33                            # IF, ELIF, ELSE
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
 
-  a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:  # break Statement
+        break
+    i += 1
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:  # continue Statement
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:  # else statement
+    print("i is no longer less than 6")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+for x in "banana":
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":  # break statement
+        break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":  # continue statement
+        continue
+    print(x)
+
+for x in range(6):  # range function
+    print(x)
+
+for x in range(2, 6):
+    print(x)
+
+for x in range(2, 30, 3):
+    print(x)
+
+for x in range(6):
+    print(x)
 else:
-  print("a is greater than b")
+    print("Finally finished!")
 
-if a > b: print("a is greater than b") 
+for x in range(6):
+    if x == 3:
+        break
+    print(x)
+else:
+    print("Finally finished!")
 
-print("A") if a > b else print("B") 
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
 
-print("A") if a > b else print("=") if a == b else print("B") 
+for x in adj:       # NESTED LOOP
+  for y in fruits:
+    print(x, y) 
 
-a = 200
-b = 33
-c = 500
-if a > b and c > a:                  #  AND
-  print("Both conditions are True")
-
-a = 200
-b = 33
-c = 500
-if a > b or a > c:                                # OR
-  print("At least one of the conditions is True")
-
-a = 33
-b = 200
-if not a > b:
-  print("a is NOT greater than b")              # NOT
-
-x = 41
-
-if x > 10:
-  print("Above ten,")
-  if x > 20:                    # NESTED IF
-    print("and also above 20!")
-  else:
-    print("but not above 20.") 
-
-a = 33
-b = 200
-
-if b > a: # PASS STATEMENT
+for x in [0, 1, 2]: # PASS statement
   pass
