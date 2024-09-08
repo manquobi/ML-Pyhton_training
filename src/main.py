ccea00b248a7a77af957c31e1a72d094d25156a6
manquobi <manquobi@gmail.com>
2024-09-08 16:05:13 +0000

    lambda

--- src/main.py
+++ src/main.py
@@ -2,102 +2,26 @@ import os
 
 os.system("cls" if os.name == "nt" else "clear")
 
-def my_function():
-  print("Hello from a function")
+x = lambda a : a + 10
+print(x(5))
 
-my_function()
+x = lambda a, b : a * b
+print(x(5, 6)) 
 
-def my_function(fname):
-  print(fname + " Refsnes")
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2)) 
 
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus") 
+def myfunc(n):
+  return lambda a : a * n
 
+mydoubler = myfunc(2)
 
-def my_function(fname, lname):
-  print(fname + " " + lname)
-
-my_function("Emil", "Refsnes") 
+print(mydoubler(11))
 
 
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
+def myfunc(n):
+  return lambda a : a * n
 
-my_function("Emil", "Tobias", "Linus")
+mytripler = myfunc(3)
 
-
-def my_function(child3, child2, child1):
-  print("The youngest child is " + child3)
-
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus") 
-
-
-def my_function(**kid):
-  print("His last name is " + kid["lname"])
-
-my_function(fname = "Tobias", lname = "Refsnes") 
-
-
-def my_function(country = "Norway"):
-  print("I am from " + country)
-
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil") 
-
-
-def my_function(food):
-  for x in food:
-    print(x)
-
-fruits = ["apple", "banana", "cherry"]
-
-my_function(fruits)
-
-
-def my_function(x):
-  return 5 * x
-
-print(my_function(3))
-print(my_function(5))
-print(my_function(9)) 
-
-def myfunction(): # pass statement
-  pass
-
-
-def my_function(x, /):
-  print(x)
-
-my_function(3) 
-
-
-def my_function(x):
-  print(x)
-
-my_function(x = 3) 
-
-
-def my_function(*, x):
-  print(x)
-
-my_function(x = 3) 
-
-def my_function(a, b, /, *, c, d):
-  print(a + b + c + d)
-
-my_function(5, 6, c = 7, d = 8) 
-
-
-def tri_recursion(k):                 # recussion
-  if(k > 0):
-    result = k + tri_recursion(k - 1)
-    print(result)
-  else:
-    result = 0
-  return result
-
-print("\n\nRecursion Example Results")
-tri_recursion(6)
+print(mytripler(11))
