a75ac22deae5910a269d848eda53fb430b240384
manquobi <manquobi@gmail.com>
2024-09-08 03:58:19 +0000

    functions

--- src/main.py
+++ src/main.py
@@ -2,84 +2,102 @@ import os
 
 os.system("cls" if os.name == "nt" else "clear")
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1
+def my_function():
+  print("Hello from a function")
 
-i = 1
-while i < 6:
-    print(i)
-    if i == 3:  # break Statement
-        break
-    i += 1
+my_function()
 
-i = 0
-while i < 6:
-    i += 1
-    if i == 3:  # continue Statement
-        continue
-    print(i)
+def my_function(fname):
+  print(fname + " Refsnes")
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1
-else:  # else statement
-    print("i is no longer less than 6")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus") 
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes") 
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus") 
+
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes") 
+
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil") 
+
+
+def my_function(food):
+  for x in food:
+    print(x)
 
 fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    print(x)
 
-for x in "banana":
-    print(x)
+my_function(fruits)
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    print(x)
-    if x == "banana":  # break statement
-        break
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    if x == "banana":
-        break
-    print(x)
+def my_function(x):
+  return 5 * x
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    if x == "banana":  # continue statement
-        continue
-    print(x)
+print(my_function(3))
+print(my_function(5))
+print(my_function(9)) 
 
-for x in range(6):  # range function
-    print(x)
-
-for x in range(2, 6):
-    print(x)
-
-for x in range(2, 30, 3):
-    print(x)
-
-for x in range(6):
-    print(x)
-else:
-    print("Finally finished!")
-
-for x in range(6):
-    if x == 3:
-        break
-    print(x)
-else:
-    print("Finally finished!")
-
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
-
-for x in adj:       # NESTED LOOP
-  for y in fruits:
-    print(x, y) 
-
-for x in [0, 1, 2]: # PASS statement
+def myfunction(): # pass statement
   pass
+
+
+def my_function(x, /):
+  print(x)
+
+my_function(3) 
+
+
+def my_function(x):
+  print(x)
+
+my_function(x = 3) 
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3) 
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8) 
+
+
+def tri_recursion(k):                 # recussion
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
