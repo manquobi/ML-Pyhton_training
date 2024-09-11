a5d388dd2b5c1c43053a8cad904bb4bef727eca7
manquobi <manquobi@gmail.com>
2024-09-11 21:28:35 +0000

    Classes

--- src/main.py
+++ src/main.py
@@ -25,3 +25,32 @@ def myfunc(n):
 mytripler = myfunc(3)
 
 print(mytripler(11))
+
+
+class MyClass:        #CLASSES/OBJECTS
+  x = 5
+print(MyClass)
+p1 = MyClass()
+print(p1.x) 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name  # Assigning name to the instance
+        self.age = age    # Assigning age to the instance
+
+p1 = Person("John", 36)  # Creating an instance of Person with name "John" and age 36
+
+print(p1.name)  # Output: John
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+p1 = Person("John", 36)
+
+print(p1)  # Output: John is 36 years old
